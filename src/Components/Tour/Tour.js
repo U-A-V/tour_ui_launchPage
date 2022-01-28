48e3c6a711be67fff3d2170fed45c37671cacb08
uav <uav0108@gmail.com>
2022-01-28 19:37:30 +0530

    created new tour componet and added a watermark to it

---
+++ src/Components/Tour/Tour.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Tour() {
+  return (
+    <div className="tour" id="tour">
+      <div className="watermark">
+        <h1>historical destination</h1>
+      </div>
+      <div className="package-container"></div>
+    </div>
+  );
+}
+
+export default Tour;
