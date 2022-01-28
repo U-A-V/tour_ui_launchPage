63273a82df439c1b2aa788e25a991eb8cdb5ba3c
uav <uav0108@gmail.com>
2022-01-28 19:29:16 +0530

    added tour section to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
+import Tour from "../src/Components/Tour/Tour";
 // import "./app.css";
 function App() {
   return (
     <div>
       <Header></Header>
       <Hero></Hero>
+      <Tour></Tour>
     </div>
   );
 }
