cc56ef680f3aa098f329bdbd5a232b07adf20aca
uav <uav0108@gmail.com>
2022-01-28 19:31:24 +0530

    added sections for packages and main action

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -5,7 +5,7 @@ import data from "../Package/packageData";
 // import "./hero.css";
 function Hero() {
   const [packages, setPackages] = useState(data);
-  useEffect(() => {}, [data]);
+  // useEffect(() => {}, [data]);
   return (
     <div className="hero">
       <div className="bg-hero">
@@ -20,7 +20,16 @@ function Hero() {
           From the depths of ocean to<span>heights of the sky</span>
         </p>
       </div>
-      <div className="packages-left"></div>
+      <div className="packages-left">
+        {packages.map((pack) => {
+          return <Package key={pack.id} pack={pack}></Package>;
+        })}
+      </div>
+      <div className="action">
+        <h3>Start Your Journy</h3>
+      </div>
+      <div className="arrow"></div>
+      <div className="right-block"></div>
     </div>
   );
 }
